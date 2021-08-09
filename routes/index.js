2cdad4780c35b0f30ce9b2603eae87b5d096fa15
Vaibhav Sai Gandi <50321348+vaibhavsaigandi@users.noreply.github.com>
2021-08-09 19:46:25 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -7,5 +7,10 @@ const { userRegisterValidator, userLoginValidator } = require('../validators/aut
 const { runValidation } = require('../validators/index')
 router.post("/signup", userRegisterValidator, runValidation, signup)
 router.post("/login", userLoginValidator, runValidation, login)
+router.get("/login", (req, res) => {
+    return res.status(200).json({
+        message: "hi prends!"
+    })
+})
 
 module.exports = router;
